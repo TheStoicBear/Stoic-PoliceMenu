f834299053cf364997303b4c82f5d95a32c28961
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-08-26 14:05:52 -0700

    Update jail_s.lua

--- source/jail/jail_s.lua
+++ source/jail/jail_s.lua
@@ -1,12 +1,14 @@
+-- Function to get player list within a radius
 lib.callback.register("getPlayerList", function(source)
+    local Framework = exports[Config.FrameworkName].getServerFunctions()
     local playerCoords = GetEntityCoords(GetPlayerPed(source))
     local players = lib.getNearbyPlayers(playerCoords, 30, false)
     local playerData = {}
 
-    for i=1, #players do
+    for i = 1, #players do
         local ply = players[i]
-        local player = NDCore.getPlayer(ply.id)
-        playerData[#playerData+1] = {
+        local player = Framework.getPlayer(ply.id)
+        playerData[#playerData + 1] = {
             name = player.fullname,
             id = ply.id
         }
@@ -17,22 +19,35 @@ lib.callback.register("getPlayerList", function(source)
     return playerData
 end)
 
+-- Event handler for jailing a player
 RegisterServerEvent('jailPlayer')
 AddEventHandler('jailPlayer', function(selectedPlayerId, jailTime, jailReason, fineAmount)
-    print("Received 'jailPlayer' event with the following parameters:")
-    print("Server: Player ID:", selectedPlayerId, "Jail Time:", jailTime, "Reason:", jailReason, "Fine:", fineAmount)
-
-    local jailedPlayer = NDCore.getPlayer(selectedPlayerId)
-    if not jailedPlayer then
+    local Framework = exports[Config.FrameworkName].getServerFunctions()
+    local player = Framework.getPlayer(selectedPlayerId)
+    
+    if not player then
         print("Failed to retrieve jailed player's information.")
         return
     end
 
-    local jailedPlayerName = jailedPlayer.firstname .. " " .. jailedPlayer.lastname
-    local success = jailedPlayer.deductMoney("bank", fineAmount, "Jail Fine")
-
-    if not success then
-        print("Failed to deduct fine from player's bank account.")
+    -- Get account info and deduct fine
+    local accountInfo = exports['money']:getaccount(selectedPlayerId)
+    if accountInfo then
+        local newBankBalance = (accountInfo.bank or 0) - fineAmount
+        if newBankBalance < 0 then
+            newBankBalance = 0  -- Prevent negative bank balance
+        end
+        local updatedAccount = {
+            cash = accountInfo.cash or 0,
+            bank = newBankBalance
+        }
+        local success = exports['money']:updateaccount(selectedPlayerId, updatedAccount)
+        if not success then
+            print("Failed to deduct fine from player's bank account.")
+            return
+        end
+    else
+        print("Failed to retrieve account information for selectedPlayerId:", selectedPlayerId)
         return
     end
 
@@ -40,22 +55,15 @@ AddEventHandler('jailPlayer', function(selectedPlayerId, jailTime, jailReason, f
     TriggerClientEvent('teleportToJail', selectedPlayerId, jailCoords)
     TriggerClientEvent('setJailedStatus', selectedPlayerId, true, jailTime)
 
-    TriggerClientEvent('chat:addMessage', selectedPlayerId, {
-        color = {255, 0, 0},
-        multiline = true,
-        args = {"Jail System", "You have been jailed for " .. jailTime .. " seconds. Reason: " .. jailReason .. ". Fine: $" .. fineAmount}
-    })
+    TriggerClientEvent('chatMessage', selectedPlayerId, "Jail System", "You have been jailed for " .. jailTime .. " seconds. Reason: " .. jailReason .. ". Fine: $" .. fineAmount)
 
-    TriggerClientEvent('chat:addMessage', source, {
-        color = {0, 255, 0},
-        multiline = true,
-        args = {"Jail System", "Player ID (" .. selectedPlayerId .. ") " .. jailedPlayerName .. " has been successfully jailed for " .. jailReason}
-    })
+    TriggerClientEvent('chatMessage', source, "Jail System", "Player ID (" .. selectedPlayerId .. ") " .. player.firstname .. " " .. player.lastname .. " has been successfully jailed for " .. jailReason)
 end)
 
-
+-- Event handler for unjailing a player
 RegisterServerEvent('unjailPlayer')
 AddEventHandler('unjailPlayer', function()
+    local Framework = exports[Config.FrameworkName].getServerFunctions()
     local src = source
     local unjailCoords = {x = 1848.86, y = 2602.36, z = 45.60}
     TriggerClientEvent('unjailPlayer', src, unjailCoords)
